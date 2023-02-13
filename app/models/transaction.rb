4200a139717e2f2ca04c0c4583a27f6697bb9526
adelguit <adeelguitoun@gmail.com>
2023-02-13 16:11:39 +0100

    Fix validation in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :category_transactions
   has_many :one_category, through: :category_transactions
-  validates :name, :amount, :category, presence: true
+  validates :name, :one_category, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
