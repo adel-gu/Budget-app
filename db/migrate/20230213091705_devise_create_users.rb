5975d1b9165fd57960313dbdc6ae62e34604eb98
adelguit <adeelguitoun@gmail.com>
2023-02-13 11:19:05 +0100

    Add app models and migrations

--- db/migrate/20230213091705_devise_create_users.rb
+++ db/migrate/20230213091705_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :full_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
