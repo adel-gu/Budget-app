f702a2821456b95035d6b384ef02fdb8356cd7cb
adelguit <adeelguitoun@gmail.com>
2023-02-13 11:23:06 +0100

    Add model associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :category_transactions
+  has_many :transactions, through: :category_transactions
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+  has_many :category_transactions
+  has_many :categories, through: :category_transactions
 end
