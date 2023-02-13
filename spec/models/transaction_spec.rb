a369907245c5b4212cec2e00efdf193ec8013b88
adelguit <adeelguitoun@gmail.com>
2023-02-13 16:11:58 +0100

    Implement tests for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Test validation
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:amount) }
+  it { should validate_presence_of(:one_category) }
+
+  # Test associations
+  it { should belong_to(:author) }
+  it { should have_many(:category_transactions) }
+  it { should have_many(:one_category).through(:category_transactions) }
+
 end
