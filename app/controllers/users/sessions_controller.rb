3ae0d48ba22b3a63520d65f0ff909b1a1d674c96
adelguit <adeelguitoun@gmail.com>
2023-02-14 10:18:36 +0100

    Add f_name key to the sign_in session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,10 +16,10 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:f_name])
+  end
 end
