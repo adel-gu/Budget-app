4ea6415410060b51621ec85ca232b09ef0c636d6
adelguit <adeelguitoun@gmail.com>
2023-02-15 14:49:20 +0100

    Fix models test implementation

--- spec/models/category_transaction_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CategoryTransaction, type: :model do
-  # Test associations
-  it { should belong_to(:one_transaction) }
-  it { should belong_to(:one_category) }
-end
