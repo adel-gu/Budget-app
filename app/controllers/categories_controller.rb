2ee8335655bf96f75e9d5d98f62fcaf04e6165fe
adelguit <adeelguitoun@gmail.com>
2023-02-15 09:47:12 +0100

    Implement the new and ceate actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,33 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
+
+  def new
+    @category = Category.new
+    respond_to do |format|
+      format.html { render :new, locals: { category: @category } }
+    end
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      format.html do
+        if @category.save
+          flash[:sucess] = 'Category Saved Successfully'
+          redirect_to categories_path
+        else
+          flash.now[:error] = 'Error: The New Category could not be saved'
+          render :new, locals: { category: @category }
+        end
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon).merge(author: current_user)
+  end
 end
