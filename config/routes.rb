a5c7ea50b2fa07cb43477e8fe8a7061181c81943
adelguit <adeelguitoun@gmail.com>
2023-02-15 11:53:58 +0100

    Add transactions reources

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   root to: "splash#index", as: :unauthenticated_user
 
-  resources :categories, only: [:index, :show, :new, :create]
+  resources :categories, only: [:index, :show, :new, :create] do
+    resources :transactions, only: [:index]
+  end
 end
